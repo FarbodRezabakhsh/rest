d93073f04bd88d43238a0fc1d4e9061bf08f274a
farbod <farbod@gmail.com>
2023-11-22 23:30:06 +0330

    add pagination and use PageNumberPagination and set it in settings.

--- A/settings.py
+++ A/settings.py
@@ -136,15 +136,15 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle',
         'rest_framework.throttling.AnonRateThrottle'
     ],
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.AdminRenderer',
-    ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon':'5/minute',
-        'user':'1/hour'
+        'anon':'20/hour',
+        'user':'20/hour'
  
         },
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
 
     
 }
